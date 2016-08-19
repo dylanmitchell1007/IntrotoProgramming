9ce9aa338d3368c8de2cd52b9fa69edbe58bb11d
dylanmitchell1007 <armadummy@yahoo.com>
2016-08-19 16:40:51 -0500

    Intial Commit
    Completed the varriable exercises.

---
+++ Project1/Source.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+int main()
+{
+	  // 2/3 + 3/5 = 5
+	 // 2%3 + 3%5 = 5
+    //   23%15% - 2 = 6
+   //   25*1/2 = 26
+  //  25*1.0/2 = 26
+ //   25*(1/2)= 26
+//   235/8 + 7 = 10
+//   ((20+1)/2-2.0)/(23 + 3)*0.2=24
+
+
+	// 2).
+	//a) AverHeight
+	//b) over $65000
+	//c) below $5000
+	//d) Steam Sale
+
+	// 3).
+	//a) integeral
+	//b) floating
+	//c) Char
+	//d) inegeral
+	//e) bool
+
+
+	// 4).
+	int A = 45;
+	int B = 25;
+	int temp = B;
+	B = A;
+	A = temp;
+
+	// 5).
+	//a) i=1 ch=a f=0
+	//b) i=1 cha=a f=0
+	//c) i=1 cha=a f=9
+	//d) i=1 ch=a f=954
+	//e) i=45 ch=a f=0.6
+	//f) i=1 ch=a f=0
+ //   int i = 0;
+	//float f = 0;
+	//char ch = 'a';
+	//std::cin >> i >> f;
+	//std::cout << "i: " << i << "\nch: " << ch << "\nf: " << f << std::endl;
+
+	
+	 // 6).
+	int i = 5 + 10 + 15 + 20 + 25;
+	int a = i / 5; 
+	std::cout << a;
+
+	system("pause");
+
+	// 7).
+	char letter = 'D';
+	int decval = letter;
+	
+	char lette = 'M';
+	int decva = lette;
+
+	char let = (letter + lette) / 2;
+		int le = let;
+		printf("%c", le);
+		system("pause");
+		return 0;
+    
+
+
+}
